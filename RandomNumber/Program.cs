9650243ba70b623fff167ab82897a57093fef5c4
TaagoT <taago.tagam@tktk.ee>
2023-09-22 16:46:04 +0300

    add Week4Loops code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+
+int Randomsum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+
+
+    myRandomNum = rnd.Next(0, 11); // 0 - 10
+    Randomsum = Randomsum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"Random sum total: {Randomsum}");
